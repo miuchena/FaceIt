62dae804bbae4c875b37de8aa075cf6c0f6b8aff
miuchena <ana.techon@gmail.com>
2017-05-30 17:07:09 +0300

    Project is added. This is all code that is writtenn at lecture 4

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Anastasia Krasnova on 30/05/2017.
+//  Copyright © 2017 Anastasia Krasnova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+
+}
+
