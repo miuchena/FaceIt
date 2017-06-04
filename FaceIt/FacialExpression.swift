94f30748d5a9f8f274be322845474d2b021a40e0
miuchena <ana.techon@gmail.com>
2017-06-04 17:55:52 +0300

    Added part that made in lecture 5

---
+++ FaceIt/FacialExpression.swift
@@ -0,0 +1,9 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by Anastasia Krasnova on 31/05/2017.
+//  Copyright © 2017 Anastasia Krasnova. All rights reserved.
+//
+
+import Foundation
